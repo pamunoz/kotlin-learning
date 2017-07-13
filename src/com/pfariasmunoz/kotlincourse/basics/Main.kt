82685429f2f7af36adf6bc23b7bbbc7ed842d47c
Pablo Munoz <pfariasmunoz@gmail.com>
2017-07-13 15:00:18 -0400

    Initial commit

---
+++ src/com/pfariasmunoz/kotlincourse/basics/Main.kt
@@ -0,0 +1,12 @@
+package com.pfariasmunoz.kotlincourse.basics
+
+import com.pfariasmunoz.kotlincourse.basics.utils.someUtility as someAditionalFunction
+
+/**
+ * Created by pablo on 08-07-17.
+ */
+fun main(args: Array<String>) {
+    println("Hello World!!!")
+
+    someAditionalFunction("Sample name")
+}
